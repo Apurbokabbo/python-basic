1ef599021b1b5f47c25a3b20a3a7723f1c61c07e
Apurbokabbo <apurbokabbo1999@gmail.com>
2026-04-19 20:03:08 +0600

    variable type

---
+++ Basic/Variables.py
@@ -0,0 +1,12 @@
+firstName ="Apurbo"
+lastName ="Kabbo"
+print(firstName + " " + lastName)
+print(type(firstName))
+
+age = 20
+print(age)
+print(type(age))
+price = 100.40
+print(price)
+print(type(price))
+
