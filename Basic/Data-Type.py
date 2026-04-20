bef86ef4ecee72c3b60a362ab75dedee3d4fcbff
Apurbokabbo <apurbokabbo1999@gmail.com>
2026-04-20 15:18:28 +0600

    data-type

---
+++ Basic/Data-Type.py
@@ -0,0 +1,39 @@
+name = "Sajjad Karim"
+print (name)
+print(type(name))
+
+
+age =27
+print(age)
+print(type(age))
+
+
+price = 100.27
+print(price)
+print(type(price))
+
+
+is_active = True
+print(is_active)
+print(type(is_active))
+
+#list
+number = [1,2,3,4,5,6]
+print(number)
+print(type(number))
+
+mixed_list = [1,2,3,"Fardin",True,]
+print(mixed_list)
+print(type(mixed_list))
+
+#dict
+statement = {
+     "name": "Karim",
+     "age": 27,
+     "mobile": "01837184946",
+     "weight": 104
+
+ }
+
+print(statement)
+print(type(statement))
--- Basic/Variables.py
+++ Basic/Variables.py
@@ -1,5 +1,5 @@
-firstName ="Apurbo"
-lastName ="Kabbo"
+firstName = "Apurbo"
+lastName = "Kabbo"
 print(firstName + " " + lastName)
 print(type(firstName))
 
@@ -9,4 +9,11 @@ print(type(age))
 price = 100.40
 print(price)
 print(type(price))
+
+
+
+#multiple
+city,zipcode,area = "Dhaka", "1216" , "Mirpur"
+x = y =z = 10
+
 
