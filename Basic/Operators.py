cf2beabb59eb572b7fa62c87f3ac4d9d8e4fa5a0
Apurbokabbo <apurbokabbo1999@gmail.com>
2026-04-20 16:02:14 +0600

    operators

---
+++ Basic/Operators.py
@@ -0,0 +1,21 @@
+# Att
+
+x =20
+y=30
+
+print(f"Addition {x} and {y} :",x+y)
+print(f"Subtraction {x} and {y} :",x-y)
+print(f"Multiplication of {x} and {y} :",x*y)
+print(f"Division of {x} and {y} :",x/y)
+print(f"Modulus of {x} and {y} :",x%y)
+
+
+#assignment operators
+
+a = 30
+b = 50
+
+print(f"Equal :",(a==b))
+print(f"Not Equal :",a==b)
+print(f"Greater Equal :",a>b)
+print(f"Less Equal :",a<b)
---
+++ Basic/Type-Casting.py
@@ -0,0 +1,20 @@
+# int to float
+x = 6
+y = float(x)
+print(f"Interger {x} to Float :",y,type(y))
+print(type(x))
+
+
+
+#float to int
+m =127.89
+n= int(m)
+print("Float to Integer",n)
+print(type(n))
+
+
+# string to int
+num_str = "100"
+num_int = int(num_str)
+print(num_int)
+print(type(num_int))
